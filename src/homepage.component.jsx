011581b8f369e4f72a28e8832a4e718848d2c43e
SushmaRamakrishna <urpalshu@gmail.com>
2020-05-10 16:01:24 -0500

    My Accounting React App

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./homepage.styles.scss";
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">PRICING</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">TAXES</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">BRANDING</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">BUSINESS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HEALTH CARE</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
